b6ac445ed4cb5e74d9635783454ebf2911de3b1a
Fatima Badaoui <fatimina615@gmail.com>
2024-04-22 15:28:15 +0200

    initialize the backend

---
+++ backend/config/connectDB.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import colors from "colors";
+
+const { MONGODB_URI } = process.env;
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(MONGODB_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.yellow.bold);
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.bold);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
