c18b1e5f5e2e1613f7197136208dc2bd662bb20b
Luke Walsh <luke.walshgt@gmail.com>
2024-04-23 13:46:13 +0200

    is waiter

--- backend/middlewares/isWaiter.js
+++ backend/middlewares/isWaiter.js
@@ -1,9 +1,13 @@
 import asyncHandler from "../config/asyncHandler.js";
 
 export const isWaiter = asyncHandler((req, res, next) => {
-    if (req.user.role !== "user" && req.user.role !== "admin" && req.user.role !== "manager") {
-      res.status(403);
-      throw new Error("Access forbidden.");
-    }
-    next();
-  });
+  if (
+    req.user.role !== "user" &&
+    req.user.role !== "admin" &&
+    req.user.role !== "manager"
+  ) {
+    res.status(403);
+    throw new Error("Access forbidden.");
+  }
+  next();
+});
