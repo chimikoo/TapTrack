fb899acd0bd511ea6235b42d0a15d3cbc35ae63d
Per-Emil Johansson <chimikoo@gmail.com>
2024-04-23 12:02:30 +0200

    exp default </3

--- backend/middlewares/isAdmin.js
+++ backend/middlewares/isAdmin.js
@@ -1,9 +1,11 @@
 import asyncHandler from "../config/asyncHandler.js";
 
-export const isAdmin = asyncHandler((req, res, next) => {
+const isAdmin = asyncHandler((req, res, next) => {
   if (req.user.role !== "admin") {
     res.status(403);
     throw new Error("Access forbidden. You must be an admin.");
   }
   next();
 });
+
+export default isAdmin;
