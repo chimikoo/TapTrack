e11eba9783644023cf17739bb0ad23bbffc1587d
Luke Walsh <luke.walshgt@gmail.com>
2024-05-22 13:47:29 +0200

    commit

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,6 +1,5 @@
 import { StatusBar } from "expo-status-bar";
 import { Image, Text, View } from "react-native";
-import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 
@@ -9,17 +8,30 @@ import InputField from "../components/InputField.jsx";
 import LoginButton from "../components/LoginButton.jsx";
 
 export default function App() {
+  const [form, setForm] = useState({ username: "", password: "" });
   return (
-    <SafeAreaView className="h-full bg-bgLight">
+    <SafeAreaView className="h-full bg-primary-lighter">
       <View className="w-full flex-1 justify-center items-center">
         <View className="w-full h-[60%] flex justify-center items-center">
           <View className="w-[75%] h-[65%] rounded-full overflow-hidden">
-            <Image source={logo} resizeMode="cover" style={{ width: '100%', height: '100%' }} />
+            <Image
+              source={logo}
+              resizeMode="cover"
+              style={{ width: "100%", height: "100%" }}
+            />
           </View>
         </View>
-        <InputField title="Username" />
-        <InputField title="Password" />
-        <LoginButton />
+        <InputField
+          title="Username"
+          value={form.username}
+          handleChange={(e) => setForm({ ...form, username: e })}
+        />
+        <InputField
+          title="Password"
+          value={form.password}
+          handleChange={(e) => setForm({ ...form, password: e })}
+        />
+        <CustomButton />
       </View>
     </SafeAreaView>
   );
