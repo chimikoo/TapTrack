16e1a7fc61f37ac3593a3c03448c77050c909c1d
Luke Walsh <luke.walshgt@gmail.com>
2024-05-22 13:07:39 +0200

    updates

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -1,11 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import InputField from "../Components/InputField.jsx";
+import logo from "../assets/Images/taptrack logo 4.png";
 
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>TapTrack!</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView className="h-full">
+    <View className="w-full flex-1 justify-center items-center">
+      <Image source={logo} resizeMode="contain" className="w-1/2" />
+
     </View>
+  </SafeAreaView>
   );
 }
