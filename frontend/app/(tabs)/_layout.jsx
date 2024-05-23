3e47027e3aea1b8d2e63e5c5e04cf1c46481e9f8
Fatima Badaoui <fatimina615@gmail.com>
2024-05-23 11:09:27 +0200

    add logout

--- frontend/app/(tabs)/_layout.jsx
+++ frontend/app/(tabs)/_layout.jsx
@@ -1,9 +1,29 @@
-import { View, Text } from "react-native";
+import { View, Text, Alert } from "react-native";
+import CustomButton from "../../components/CustomButton.jsx";
+import axios from "axios";
+import { router } from "expo-router";
 
 const TabsLayout = () => {
+  const logout = async () => {
+    try {
+      const { data } = await axios.get(
+        "https://application-server.loca.lt/users/logout"
+      );
+      console.log("data", data);
+      router.replace("/");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    }
+  };
+
   return (
     <View>
       <Text>TabsLayout</Text>
+      <CustomButton
+        text="Log Out"
+        containerStyles="w-[80%] mt-4"
+        handlePress={logout}
+      />
     </View>
   );
 };
