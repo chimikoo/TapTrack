edeb3a22cf39a6e98e66dd1e1bec2e7f72e80ad6
Luke Walsh <luke.walshgt@gmail.com>
2024-06-03 14:29:04 +0200

    timetrack added

---
+++ frontend/components/timeTrack.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import axios from "axios";
+
+const TimeTrack = () => {
+  const [timeTrackData, setTimeTrackData] = useState([]);
+
+  useEffect(() => {
+    // Fetch time track data for a user
+    const fetchTimeTrackData = async () => {
+      setLoading(true);
+      const url = `${TAP_TRACK_URL}/users/timeTrack/${id}`;
+      try {
+        const { data } = await axios.get(url);
+        setTimeTrackData(data.data);
+      } catch (error) {
+        console.error("Error fetching time track data:", error);
+        setError(error.message);
+      }
+      setLoading(false);
+    };
+    fetchTimeTrackData();
+  }, [id]);
+
+  return (
+    <View>
+      <Text>Time Track</Text>
+      <TouchableOpacity onPress={() => console.log("Add time")}>
+        <Text>Add time</Text>
+      </TouchableOpacity>
+      {loading && <Text>Loading...</Text>}
+      {error && <Text style={{ color: "red" }}>{error}</Text>}
+      {timeTrackData && (
+        <View>
+          {Object.entries(timeTrackData.months).map(
+            ([keyName, item], index) => (
+              <View key={index}>
+                <Text>{keyName}</Text>
+                <Text>{item.monthlyTotal.hours}</Text>
+                <Text>{item.monthlyTotal.minutes}</Text>
+                <View>
+                  {item.shifts.map((shift, shiftIndex) => (
+                    <View key={shiftIndex}>
+                      <Text>{shift.day}</Text>
+                      <Text>{shift.total.hours}</Text>
+                      <Text>{shift.total.minutes}</Text>
+                    </View>
+                  ))}
+                </View>
+              </View>
+            )
+          )}
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default TimeTrack;
