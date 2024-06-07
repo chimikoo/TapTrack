edecfcc9f2dc140975a115df064f2309d4241e02
Fatima Badaoui <fatimina615@gmail.com>
2024-06-07 10:18:29 +0200

    resolved error in orderDetail

--- frontend/app/(tabs)/(menu)/orderDetail.jsx
+++ frontend/app/(tabs)/(menu)/orderDetail.jsx
@@ -1,11 +1,5 @@
 import { useLocalSearchParams } from "expo-router";
-import {
-  View,
-  Text,
-  ScrollView,
-  FlatList,
-  ActivityIndicator,
-} from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import OrderItemsMap from "../../../components/OrderItemsMap.jsx";
 import CustomButton from "../../../components/CustomButton.jsx";
@@ -16,7 +10,6 @@ import axios from "axios";
 const OrderDetail = () => {
   const { orderId } = useLocalSearchParams();
   const [order, setOrder] = useState({});
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getOrder = async () => {
@@ -24,22 +17,17 @@ const OrderDetail = () => {
         const url = `${TAP_TRACK_URL}/users/menu-orders/${orderId}`;
         const { data } = await axios.get(url);
         setOrder(data.data);
-        setLoading(false);
       } catch (error) {
-        console.error("error", error);
+        console.log("Error fetching order", error);
       }
     };
     getOrder();
   }, []);
 
-  console.log("order", order);
-
   return (
     <SafeAreaView>
-      {loading ? (
-        <ActivityIndicator size="large" color="#7CA982" />
-      ) : (
-        <>
+      <>
+        {order && (
           <ScrollView
             className={`w-[90%] m-auto rounded-lg p-6 mb-4 ${
               order.isCheckout ? "bg-primary" : "bg-secondary"
@@ -57,11 +45,17 @@ const OrderDetail = () => {
               <Text className="text-myWhite">Table N°</Text>
               <Text className="text-myWhite">{order.tableNumber}</Text>
             </View>
-            <OrderItemsMap title="Drinks" items={order.drinks} />
-            <OrderItemsMap title="Starters" items={order.starter} />
-            <OrderItemsMap title="Mains" items={order.main} />
-            <OrderItemsMap title="Sides" items={order.side} />
-            <OrderItemsMap title="Desserts" items={order.dessert} />
+            {order.drinks && (
+              <OrderItemsMap title="Drinks" items={order.drinks} />
+            )}
+            {order.starter && (
+              <OrderItemsMap title="Starters" items={order.starter} />
+            )}
+            {order.main && <OrderItemsMap title="Mains" items={order.main} />}
+            {order.side && <OrderItemsMap title="Sides" items={order.side} />}
+            {order.dessert && (
+              <OrderItemsMap title="Desserts" items={order.dessert} />
+            )}
             <View className="flex-row justify-between py-4 mb-10">
               <Text className="text-myWhite">Date</Text>
               <Text className="text-myWhite">
@@ -69,17 +63,17 @@ const OrderDetail = () => {
               </Text>
             </View>
           </ScrollView>
-          {!order.isCheckout && (
-            <CustomButton
-              text="Checkout"
-              containerStyles="w-[50%] m-auto mb-4"
-              handlePress={() => {
-                console.log("Pay button pressed");
-              }}
-            />
-          )}
-        </>
-      )}
+        )}
+        {!order.isCheckout && (
+          <CustomButton
+            text="Checkout"
+            containerStyles="w-[50%] m-auto mb-4"
+            handlePress={() => {
+              console.log("Pay button pressed");
+            }}
+          />
+        )}
+      </>
     </SafeAreaView>
   );
 };
