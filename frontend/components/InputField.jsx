a4f76a6322c76c283e126d468200372790c905d5
Fatima Badaoui <fatimina615@gmail.com>
2024-06-12 13:23:14 +0200

    add theme context to profile

--- frontend/components/InputField.jsx
+++ frontend/components/InputField.jsx
@@ -2,22 +2,38 @@ import { useState } from "react";
 import { View, TextInput, TouchableOpacity, Image } from "react-native";
 import eye from "../assets/icons/eye.png";
 import eyeHide from "../assets/icons/eye-hide.png";
+import { useTheme } from "../contexts/themeContext.jsx";
 
 const InputField = ({ title, value, handleChange }) => {
   const [showPassword, setShowPassword] = useState(false);
+  const { theme } = useTheme();
+
+  const bgColor =
+    theme === "light"
+      ? "border-primary-dark bg-myWhite"
+      : "border-primary-light bg-opacGray";
 
   return (
     <View className="space-y-2 w-[80%] py-2 ">
-      <View className="w-full h-[6vh] px-4 rounded-lg border border-primary-dark flex flex-row items-center justify-between bg-myWhite ">
+      <View
+        className={`w-full h-[6vh] px-4 rounded-lg flex flex-row items-center justify-between border ${bgColor}`}
+      >
         <TextInput
           value={value}
           placeholder={title}
           placeholderTextColor="#8e8e8e"
-          secureTextEntry={(title === "Password" || title === "New Password" || title === "Confirm new password") && !showPassword}
+          secureTextEntry={
+            (title === "Password" ||
+              title === "New Password" ||
+              title === "Confirm new password") &&
+            !showPassword
+          }
           onChangeText={handleChange}
           className="flex-1"
         />
-        {(title === "Password" || title === "New Password" || title === "Confirm new password") && (
+        {(title === "Password" ||
+          title === "New Password" ||
+          title === "Confirm new password") && (
           <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
             <Image
               source={showPassword ? eyeHide : eye}
