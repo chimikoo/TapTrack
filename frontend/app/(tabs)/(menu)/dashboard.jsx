e251f0326e127eb2e2cb5c8343b345facbd0827f
Fatima Badaoui <fatimina615@gmail.com>
2024-06-18 09:22:04 +0200

    add logic to generate EoD

--- frontend/app/(tabs)/(menu)/dashboard.jsx
+++ frontend/app/(tabs)/(menu)/dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, ScrollView, TouchableOpacity } from "react-native";
+import { View, Text, ScrollView, TouchableOpacity, Alert } from "react-native";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from "react-native-vector-icons/FontAwesome";
 import MyBarChart from "../../../components/BarChart";
 import CustomButton from "../../../components/CustomButton";
 import axios from "axios";
@@ -10,7 +10,8 @@ import { TAP_TRACK_URL } from "@env";
 export default function DashboardScreen() {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
-  const [isStartDatePickerVisible, setStartDatePickerVisibility] = useState(false);
+  const [isStartDatePickerVisible, setStartDatePickerVisibility] =
+    useState(false);
   const [isEndDatePickerVisible, setEndDatePickerVisibility] = useState(false);
   const [receipts, setReceipts] = useState([]);
 
@@ -18,17 +19,20 @@ export default function DashboardScreen() {
 
   const fetchData = async () => {
     try {
-      console.log('Fetching data...');
+      console.log("Fetching data...");
       console.log(`Start Date: ${startDate.toISOString()}`);
       console.log(`End Date: ${endDate.toISOString()}`);
-      const { data } = await axios.get(`${TAP_TRACK_URL}/eod/receipts/date-range`, {
-        params: {
-          startDate: startDate.toISOString(),
-          endDate: endDate.toISOString(),
-        },
-      });
+      const { data } = await axios.get(
+        `${TAP_TRACK_URL}/eod/receipts/date-range`,
+        {
+          params: {
+            startDate: startDate.toISOString(),
+            endDate: endDate.toISOString(),
+          },
+        }
+      );
       setReceipts(data.data);
-      console.log('Data fetched:', data.data);
+      console.log("Data fetched:", data.data);
     } catch (error) {
       console.log(error);
     }
@@ -65,8 +69,8 @@ export default function DashboardScreen() {
   };
 
   const formatDate = (date) => {
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
     const year = date.getFullYear();
     return `${day}-${month}-${year}`;
   };
@@ -74,7 +78,7 @@ export default function DashboardScreen() {
   const aggregateData = (receipts) => {
     const dailyRevenue = {};
 
-    receipts.forEach(receipt => {
+    receipts.forEach((receipt) => {
       const date = new Date(receipt.transactionDate);
       if (!dailyRevenue[date]) {
         dailyRevenue[date] = 0;
@@ -82,7 +86,7 @@ export default function DashboardScreen() {
       dailyRevenue[date] += receipt.totalAmount;
     });
 
-    return Object.keys(dailyRevenue).map(date => ({
+    return Object.keys(dailyRevenue).map((date) => ({
       date,
       totalRevenue: dailyRevenue[date],
     }));
@@ -91,18 +95,18 @@ export default function DashboardScreen() {
   const aggregatedData = aggregateData(receipts);
 
   const totalOrders = receipts.length;
-  const totalUnpaid = receipts.filter(receipt => !receipt.isPaid).length;
+  const totalUnpaid = receipts.filter((receipt) => !receipt.isPaid).length;
   const totalPaid = totalOrders - totalUnpaid;
 
-  const paidReceipts = receipts.filter(receipt => receipt.isPaid);
+  const paidReceipts = receipts.filter((receipt) => receipt.isPaid);
 
   const totalCashRevenue = paidReceipts
-    .filter(receipt => receipt.paymentMethod === 'Cash')
+    .filter((receipt) => receipt.paymentMethod === "Cash")
     .reduce((acc, receipt) => acc + receipt.totalAmount, 0)
     .toFixed(2);
 
   const totalCardRevenue = paidReceipts
-    .filter(receipt => receipt.paymentMethod === 'Credit Card')
+    .filter((receipt) => receipt.paymentMethod === "Credit Card")
     .reduce((acc, receipt) => acc + receipt.totalAmount, 0)
     .toFixed(2);
 
@@ -111,33 +115,62 @@ export default function DashboardScreen() {
     .toFixed(2);
 
   const totalLoss = receipts
-    .filter(receipt => !receipt.isPaid)
+    .filter((receipt) => !receipt.isPaid)
     .reduce((acc, receipt) => acc + receipt.totalAmount, 0)
     .toFixed(2);
 
   const totalFoodSold = receipts.reduce((acc, receipt) => {
-    const starterItems = receipt.items.filter(item => item.category === 'starter');
-    const mainItems = receipt.items.filter(item => item.category === 'main');
-    const sideItems = receipt.items.filter(item => item.category === 'side');
-    const dessertItems = receipt.items.filter(item => item.category === 'dessert');
-    return acc + starterItems.length + mainItems.length + sideItems.length + dessertItems.length;
+    const starterItems = receipt.items.filter(
+      (item) => item.category === "starter"
+    );
+    const mainItems = receipt.items.filter((item) => item.category === "main");
+    const sideItems = receipt.items.filter((item) => item.category === "side");
+    const dessertItems = receipt.items.filter(
+      (item) => item.category === "dessert"
+    );
+    return (
+      acc +
+      starterItems.length +
+      mainItems.length +
+      sideItems.length +
+      dessertItems.length
+    );
   }, 0);
 
   const totalDrinksSold = receipts.reduce((acc, receipt) => {
-    const drinkItems = receipt.items.filter(item => item.category === 'beverage');
+    const drinkItems = receipt.items.filter(
+      (item) => item.category === "beverage"
+    );
     return acc + drinkItems.length;
   }, 0);
+
+  const generateEod = async () => {
+    try {
+      console.log("Generating EoD...");
+      const { data } = await axios.post(`${TAP_TRACK_URL}/eod`);
+      console.log("EoD generated:", data);
+      Alert.alert("Success", "EoD generated successfully!");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <View className="flex-1 bg-primary-lighter">
       <ScrollView className="flex-1 pt-5 pl-2 pr-2">
         <View className="bg-primary-lighter shadow p-2">
           <View className="flex-row justify-between mb-4">
-            <TouchableOpacity className="flex-row items-center p-2 border border-black rounded" onPress={showStartDatePicker}>
+            <TouchableOpacity
+              className="flex-row items-center p-2 border border-black rounded"
+              onPress={showStartDatePicker}
+            >
               <Text className="mr-2 text-lg">{formatDate(startDate)}</Text>
               <Icon name="calendar" size={20} color="#6D9773" />
             </TouchableOpacity>
-            <TouchableOpacity className="flex-row items-center p-2 border border-black rounded" onPress={showEndDatePicker}>
+            <TouchableOpacity
+              className="flex-row items-center p-2 border border-black rounded"
+              onPress={showEndDatePicker}
+            >
               <Text className="mr-2 text-lg">{formatDate(endDate)}</Text>
               <Icon name="calendar" size={20} color="#6D9773" />
             </TouchableOpacity>
@@ -179,15 +212,11 @@ export default function DashboardScreen() {
               </View>
               <View className="justify-center flex-1 bg-primary-light p-3">
                 <Text className="text-center font-bold text-xs">Cash</Text>
-                <Text className="text-center text-xs">
-                  {totalCashRevenue}€
-                </Text>
+                <Text className="text-center text-xs">{totalCashRevenue}€</Text>
               </View>
               <View className="justify-center flex-1 bg-primary-light p-3">
                 <Text className="text-center font-bold text-xs">Card</Text>
-                <Text className="text-center text-xs">
-                  {totalCardRevenue}€
-                </Text>
+                <Text className="text-center text-xs">{totalCardRevenue}€</Text>
               </View>
               <View className="justify-center flex-1 bg-primary-light p-3">
                 <Text className="text-center font-bold">Loss</Text>
@@ -196,9 +225,7 @@ export default function DashboardScreen() {
             </View>
             <View className="flex-row justify-between mt-4">
               <View className="flex-1 mr-2 bg-primary p-2">
-                <Text className="text-center font-bold text-xs">
-                  Food sold
-                </Text>
+                <Text className="text-center font-bold text-xs">Food sold</Text>
                 <Text className="text-center">{totalFoodSold}</Text>
               </View>
               <View className="flex-1 ml-2 bg-primary p-2">
@@ -212,7 +239,11 @@ export default function DashboardScreen() {
         </View>
         <MyBarChart eods={aggregatedData} />
         <TouchableOpacity className="p-2">
-          <CustomButton text="Generate EoD" containerStyles={"mb-5"} />
+          <CustomButton
+            text="Generate EoD"
+            containerStyles={"mb-5"}
+            handlePress={generateEod}
+          />
         </TouchableOpacity>
       </ScrollView>
     </View>
